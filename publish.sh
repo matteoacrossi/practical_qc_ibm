85e80be1ae878715067d19d3b232c33346e3eef1
Matteo Rossi <teo.red90@gmail.com>
2021-04-07 19:21:26 +0300

    Update

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+jupyter-nbconvert --to slides $1.ipynb
+mv $1.slides.html  index.html
+mkdir -p /tmp/workspace
+cp -r * /tmp/workspace/
+git add -A .
+git commit -m "Update"
+git checkout -B gh-pages
+cp -r /tmp/workspace/* .
+git add -A .
+git commit -m "new version"
+git push origin master gh-pages
+git checkout master
+rm -rf /tmp/workspace
